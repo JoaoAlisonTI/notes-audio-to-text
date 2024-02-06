e686c6bc6c0f8c141bb9dd27dc62721e2f616f8f
JoaoAlisonTI <joaoalison.ti@gmail.com>
2024-02-06 16:47:37 -0300

    feat[App main]: initial setup on project: Initial styles with tailwind, and components from the app;

---
+++ src/components/note-card.tsx
@@ -0,0 +1,10 @@
+export function NoteCard() {
+  return (
+    <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 outline-none overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+      <span className="text-sm font-medium text-slate-300">Há 2 dias</span>
+      <p className="text-sm leading-6 text-slate-400">Iniciar o nlw, essa semana vai ser show.Iniciar o nlw, essa semana vai ser show. Iniciar o nlw, essa semana vai ser show.Iniciar o nlw, essa semana vai ser show. Iniciar o nlw, essa semana vai ser show.Iniciar o nlw, essa semana vai ser show.</p>
+          
+      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+    </button>
+  )
+}
